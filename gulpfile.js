67b0fae5b367722fd73c2200ecbb366c504f9f4a
wishmaster <wishmaster2310@gmail.com>
2016-03-27 19:10:40 +0500

    index page

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+var path = require('path');
+var combiner = require('stream-combiner2');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var less = require('gulp-less');
+var server = require('gulp-express');
+var nunjucksRender = require('gulp-nunjucks-render');
+var prettify = require('gulp-html-prettify');
+var replace = require('gulp-replace');
+var spritesmith = require('gulp.spritesmith');
+var merge = require('merge-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var siteDB = require('./datasource/sdata.json');
+var LessPluginAutoPrefix = require('less-plugin-autoprefix');
+var LessPluginCleanCSS = require('less-plugin-clean-css');
+siteDB.static_path = "public"
+gulp.task('less:dev', function() {
+    var autoprefix = new LessPluginAutoPrefix({
+      browsers: ["last 2 versions"]
+    });
+
+  return gulp.src('public/less/style.less')
+  	//.pipe(sourcemaps.init())
+    .pipe(less({
+      plugins: [autoprefix]
+    }).on('error', function(err) {
+      gutil.log(err);
+      this.emit('end');
+    }))
+    //.pipe(sourcemaps.write('.', {includeContent: false, mapSources: 'public/less/**'}))
+    .pipe(gulp.dest('public/stylesheets/'));
+});
+
+gulp.task('less:prod', function() {
+  var cleancss = new LessPluginCleanCSS({
+      advanced: true
+    }),
+    autoprefix = new LessPluginAutoPrefix({
+      browsers: ["last 30 versions", "IE 8", "IE 9"]
+    });
+
+  return gulp.src('public/less/style.less')
+    //.pipe(sourcemaps.init())
+    .pipe(less({
+      plugins: [autoprefix, cleancss]
+    }).on('error', function(err) {
+      gutil.log(err);
+      this.emit('end');
+    }))
+    //.pipe(sourcemaps.write('.', {includeContent: false, mapSources: 'public/less/**'}))
+    .pipe(gulp.dest('public/stylesheets/'));
+});
+
+gulp.task('sprites', function() {
+
+  var spriteData = gulp.src('public/images/__icons/*.png').pipe(spritesmith({
+    imgName: 'c-icon-sprite.png',
+    cssName: 'c-icon.less',
+    padding: 10,
+    cssTemplate: 'main-sprite.css.hbs'
+  }));
+
+  var imgStream = spriteData.img
+    .pipe(gulp.dest('public/images/'));
+  var cssStream = spriteData.css
+    .pipe(gulp.dest('public/less/'));
+
+  return merge(imgStream, cssStream);
+});
+
+gulp.task('default', function() {
+ 
+  server.run(['bin/www'], [], [true]);
+
+  gulp.watch(['views/**/*.html', 'views/layouts/*.html', 'views/components/*.html','views/*.html', './datasource/sdata.json', './app.js', 'routes/**/*.js'], function(event) {
+    server.notify(event);
+    console.log(path.basename(event.path), 'was', event.type);
+    server.stop();
+    server.run(['bin/www'])
+  });
+  gulp.watch(['public/images/__icons/*.png'], ['sprites', server.notify]);
+  gulp.watch(['public/less/*.less', 'public/less/**/*.less'], ['less:dev', server.notify]);
+});
+
+
+gulp.task('export', function() {
+  nunjucksRender.nunjucks.configure(['views/'], {
+    watch: false
+  });
+
+  
+  gulp.src(['views/*.html', '!views/__*.html'])
+    .pipe(nunjucksRender({
+      ctx: siteDB
+    }))
+    .pipe(prettify({
+      indent_char: ' ',
+      indent_size: 2
+    }))
+    .pipe(gulp.dest('export'));
+});
+
+
+gulp.task('publish', ['export', 'less:prod']);
---
+++ public/javascripts/general.js
@@ -0,0 +1,72 @@
+$(function() {
+	
+
+	$('.c-slider').slick({
+		dots: false,
+		nextArrow: '<div class="c-slider__arrow c-slider__arrow--next"></div>',
+		prevArrow: '<div class="c-slider__arrow c-slider__arrow--prev"></div>'
+	})
+
+	$('.c-wtf__slider')
+		.slick({
+			dots: false,
+			arrows: false,
+			infinite: false,
+			fade: true,
+			draggable: false
+			//swipe: false
+		})
+		.on('afterChange', function(e, slick, currentSlide) {
+				console.log(currentSlide)
+				var menu = $('.c-wtf__menu-item[data-slide='+ currentSlide +']');
+				menu.addClass('c-wtf__menu-item--active')
+		})
+		.on('beforeChange', function(e, slick, currentSlide) {
+			$('.c-wtf__menu-item--active').removeClass('c-wtf__menu-item--active')
+		});
+	$('.c-wtf__menu-item').first().addClass('c-wtf__menu-item--active');
+		
+	$('.c-wtf__menu-item').on('click', function(e) {
+		e.preventDefault();
+		$('.c-wtf__slider').slick('slickGoTo', $(this).attr('data-slide'));
+		$(this).addClass('c-wtf__menu-item--active');
+	});
+
+	$('.c-map__toggler').on('click', function(e) {
+		e.preventDefault();
+		$(this).closest('.c-map__block').addClass('c-map__block--active')
+	});
+
+	$('.c-map__form-close').on('click', function(e) {
+		e.preventDefault();
+		$(this).closest('.c-map__block').removeClass('c-map__block--active')
+	})
+
+	var myMap;
+
+	
+	ymaps.ready(init);
+
+	function init () {
+	    
+   	var myMap = new ymaps.Map('yamap', {
+      //center:[56.843417, 60.575493], // Москва
+      center:[56.843517, 60.573733], // Москва
+      zoom:17
+    }, {
+      suppressMapOpenBlock: true
+    }),
+
+   	myPlacemark = new ymaps.Placemark([56.843417, 60.575493], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: '/images/map-pin.png',
+      iconImageSize: [165, 55],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-86, -65]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+	}
+
+});
